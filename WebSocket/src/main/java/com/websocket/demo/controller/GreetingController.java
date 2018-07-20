85429bf366ef7c31345b7f4a081ecc9dd9e1ae2b
i3oys1205 <sowrd_two@naver.com>
2018-07-20 13:36:17 +0900

    install in

---
+++ WebSocket/src/main/java/com/websocket/demo/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.websocket.demo.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.websocket.demo.Greeting;
+import com.websocket.demo.HelloMessage;
+
+@Controller  // 메시지를 수신해서 처리하는 클래스 정의.
+public class GreetingController {
+
+    @MessageMapping("/hello")  // 요청 토픽이 "/hello"일 때 처리할 함수. 
+    @SendTo("/topic/greetings")  // 응답을 보낼 토픽 주소
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        
+        // Greeting 클래스로 응답을 전달. json 형태로 전달됨.
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+}
+
